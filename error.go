cec58f5acb90b95a579d6e88dc057a4acf3c4df5
kas2000 <kyzaibekk@gmail.com>
2022-01-18 00:09:50 +0600

    feat: http error system

---
+++ error.go
@@ -0,0 +1,28 @@
+package http
+
+import (
+	"net/http"
+	"strconv"
+)
+
+type Error struct {
+	Status  int    `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+	Code    string `json:"code,omitempty"`
+	System  string `json:"system,omitempty"`
+}
+
+func NewError(status int, message string, system string, code int) *Error {
+	c := strconv.Itoa(status) + strconv.Itoa(code)
+	return &Error{
+		System:  system,
+		Status:  status,
+		Message: message,
+		Code:    c,
+	}
+}
+
+func BadRequest(code int, message string, system string) Response {
+	err := NewError(http.StatusBadRequest, message, system, code)
+	return NewResponse(http.StatusBadRequest, err, nil)
+}
